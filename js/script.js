f2ae9b03e00a92d8450c4130319fdaf567af12e1
Francesco-Matteucci <francescomatteucci@gmail.com>
2024-09-03 14:10:09 +0200

    added title with vue

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
+
+
+// Creo la nuova istanza Vue
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Benvenuto, finalmente, in VUE!'
+        }
+    }
+}).mount('#app')
